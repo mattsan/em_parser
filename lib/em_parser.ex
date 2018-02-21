d72d9446bf57bad83a4bebc157f3d5341b550d4a
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-02-21 09:25:23 +0900

    initial commit

---
+++ lib/em_parser.ex
@@ -0,0 +1,18 @@
+defmodule EMParser do
+  @moduledoc """
+  Documentation for EMParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EMParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/em_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule EMParserTest do
+  use ExUnit.Case
+  doctest EMParser
+
+  test "greets the world" do
+    assert EMParser.hello() == :world
+  end
+end
